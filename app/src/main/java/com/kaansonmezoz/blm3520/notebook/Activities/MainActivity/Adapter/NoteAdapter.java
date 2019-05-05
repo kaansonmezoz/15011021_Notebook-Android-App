b3626a4cbbe679162c4d8844d8ff2c4fc1d6e066
Kaan <ksonmezoz@outlook.com>
2019-05-05 19:13:39 +0300

    Click listeners have been added to the note items

--- app/src/main/java/com/kaansonmezoz/blm3520/notebook/Activities/MainActivity/Adapter/NoteAdapter.java
+++ app/src/main/java/com/kaansonmezoz/blm3520/notebook/Activities/MainActivity/Adapter/NoteAdapter.java
@@ -1,7 +1,10 @@
 package com.kaansonmezoz.blm3520.notebook.Activities.MainActivity.Adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,14 +15,33 @@ import com.kaansonmezoz.blm3520.notebook.R;
 import java.util.List;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
-    private List<NoteItem> noteItems;
+    private  List<NoteItem> noteItems;
 
     public NoteAdapter(List<NoteItem> noteItems){
         this.noteItems = noteItems;
     }
 
     public NoteAdapter.NoteViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_note_item, parent, false);
+        final View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_note_item, parent, false);
+
+        //itemView.createContextMenu();
+
+        itemView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+            //TODO: Related information about the clicked note must be shown to the user
+            }
+        });
+
+
+
+        /*
+        itemView.setOnLongClickListener(new View.OnLongClickListener(){
+            public boolean onLongClick(View view){
+                //TODO: options: Select, Delete, Set Reminder, Share
+            }
+        });
+        */
+
 
         return new NoteViewHolder(itemView);
     }
@@ -30,19 +52,71 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         holder.firstCharacters.setText(noteItem.getTruncatedWords());
         holder.title.setText(noteItem.getTitle());
         holder.lastUpdateDate.setText(noteItem.getLastUpdatedDate());
+
     }
 
     public int getItemCount(){
         return noteItems.size();
     }
 
-    public static class NoteViewHolder extends RecyclerView.ViewHolder{
+    public void removeAt(int position) {
+    }
+
+    public  class NoteViewHolder extends RecyclerView.ViewHolder{
         public TextView title;
         public TextView lastUpdateDate;
         public TextView firstCharacters;
 
         public NoteViewHolder(View view){
             super(view);
+
+                                                    //TODO: ContextMenu yerine belki de popup olusturmak daha mantikli olabilir ?
+
+            //todo: kod acayip bir sekilde cirkinlesmeye basladi bunu bir guzel refactor etmek lazim
+            view.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener(){     //TODO: Bunun alternatiflerine de bakmak da yarar var
+                public void onCreateContextMenu(ContextMenu menu, final View view, ContextMenu.ContextMenuInfo menuInfo){
+
+                    menu.add("Select").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            Log.d("MainActivity", "Select clicked");
+                            return false;
+                        }
+                    });
+
+                    menu.add("Delete").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            int position = getAdapterPosition();
+
+                            noteItems.remove(position);
+                            notifyItemRemoved(position);
+                            notifyItemRangeChanged(position, noteItems.size());
+
+
+                            Log.d("MainActivity", "Delete clicked");
+                            return false;
+                        }
+                    });
+
+                    menu.add("Set Reminder").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            Log.d("MainActivity", "Set Reminder clicked");
+                            return false;
+                        }
+                    });
+
+                    menu.add("Share").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            Log.d("MainActivity", "Share clicked");
+                            return false;
+                        }
+                    });
+                }
+
+            });
 
             title = view.findViewById(R.id.note_title);
             lastUpdateDate = view.findViewById(R.id.note_last_update_date);
--- app/src/main/java/com/kaansonmezoz/blm3520/notebook/Activities/MainActivity/MainActivity.java
+++ app/src/main/java/com/kaansonmezoz/blm3520/notebook/Activities/MainActivity/MainActivity.java
@@ -1,9 +1,16 @@
 package com.kaansonmezoz.blm3520.notebook.Activities.MainActivity;
 
+import android.animation.LayoutTransition;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.kaansonmezoz.blm3520.notebook.Activities.MainActivity.Adapter.NoteAdapter;
+import com.kaansonmezoz.blm3520.notebook.Activities.MainActivity.Model.NoteItem;
 import com.kaansonmezoz.blm3520.notebook.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.note_recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<NoteItem> noteItems = new ArrayList<NoteItem>();
+
+        noteItems.add(new NoteItem(1, "deneme", "deneme",   "deneme"));
+        noteItems.add(new NoteItem(1, "deneme1", "deneme1",   "deneme1"));
+        noteItems.add(new NoteItem(1, "deneme2", "deneme",   "deneme"));
+        noteItems.add(new NoteItem(1, "deneme3", "deneme",   "deneme"));
+        noteItems.add(new NoteItem(1, "deneme4", "deneme",   "deneme"));
+        noteItems.add(new NoteItem(1, "deneme5", "deneme",   "deneme"));
+        noteItems.add(new NoteItem(1, "deneme6", "deneme",   "deneme"));
+        noteItems.add(new NoteItem(1, "deneme7", "deneme",   "deneme"));
+
+
+        NoteAdapter noteAdapter = new NoteAdapter(noteItems);
+        recyclerView.setAdapter(noteAdapter);
     }
 }
